402fad1c9ee7a59700ac20cb038db47423c5540b
Fred <guru.three@gmail.com>
2014-02-25 11:10:28 +0000

    Article discussing the AirPi sensors

---
+++ content/images/environment/sensors/ldr/ldr.m
@@ -0,0 +1,45 @@
+log(100)/log(2.8)
+log(1)/log(60)
+log(1)
+log(2)/log(40)
+x = [1 2 100]
+y = [60 40 2.8]
+plot(x, y)
+loglog(x, y)
+plot(log(x), log(y))
+loglog(x, y)
+log(x)
+x
+plot(log(x), log(y))
+polyfit
+help polyfit
+polyfit(log(x), log(y), 1)
+t = ans
+polyval
+help polyval
+plot([1:100], polyval([1:100], t))
+plot([1:100], polyval((t, [1:100]))
+plot([1:100], polyval(t, [1:100]))
+plot(log([1:100]), log(polyval(t, [1:100])))
+plot([1:100], polyval(t, [1:100]))
+plot(log(x), log(y))
+figure
+plot([1:5], polyval(t, [1:5]))
+plot(exp([1:5]), exp(polyval(t, [1:5])))
+hold on
+plot(x, y, 'o')
+hold off
+t
+exp([1:5
+])
+x
+ln(x)
+log(x)
+y
+exp((ln(40)-4.125)/-0.6704)
+exp((log(40)-4.125)/-0.6704)
+exp((log(y)-4.125)/-0.6704)
+x
+polyfit(log(x), log(y), 1)
+x
+y
