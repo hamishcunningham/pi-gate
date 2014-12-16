85cf1f059072600690606f1e69e035c79a852ed8
Hamish Cunningham <hamish@gate.ac.uk>
2014-12-16 14:28:24 +0000

    added script to get list of files uploaded to s3

---
+++ bin/get-upload-list.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# get-upload-list.sh
+
+# standard locals
+alias cd='builtin cd'
+P="$0"
+USAGE="`basename ${P}` [-h(elp)] [-d(ebug)]"
+DBG=:
+OPTIONSTRING=hd
+
+# specific locals
+INSTANCE=0
+DNS=""
+USEDNS="1"
+
+# message & exit if exit num present
+usage() { echo -e Usage: $USAGE; [ ! -z "$1" ] && exit $1; }
+
+# process options
+while getopts $OPTIONSTRING OPTION
+do
+  case $OPTION in
+    h)	usage 0 ;;
+    d)	DBG=echo ;;
+    *)	usage 1 ;;
+  esac
+done 
+shift `expr $OPTIND - 1`
+
+sed -n '/^s3cmd /,$p'
